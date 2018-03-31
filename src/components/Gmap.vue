d37a056777069e858baa3d1acadb85d38f04cbb4
Anton <repushko.a@gmail.com>
2018-03-31 18:28:36 +0300

    Test markers

--- src/components/Gmap.vue
+++ src/components/Gmap.vue
@@ -1,21 +1,60 @@
 <template>
-	<div id='gmap'>
-		<gmap-map :center="{lat:10, lng:10}" :zoom="7" map-type-id="terrain" style="width: 500px; height: 300px"></gmap-map>
-	</div>
+    <div id='gmap'>
+        <gmap-map :center="center" :options="options" :zoom="14" map-type-id="terrain" style="width: 1000px; height: 700px">
+            <gmap-marker :key="index" v-for="(m, index) in markers" :position="m.position" :clickable="true" :draggable="false" @click="center=m.position"></gmap-marker>
+        </gmap-map>
+    </div>
 </template>
-
 <script>
 export default {
 
-  name: 'Gmap',
+    name: 'Gmap',
 
-  data () {
-    return {
-
+    data() {
+        return {
+            center: {
+                lat: 54.1911,
+                lng: 37.6143
+            },
+            markers: [{
+                position: {
+                    lat: 54.1853,
+                    lng: 37.6775
+                }
+            }, {
+                position: {
+                    lat: 54.2017,
+                    lng: 37.7092
+                }
+            }, {
+                position: {
+                    lat: 54.1686,
+                    lng: 37.7097
+                }
+            }, {
+                position: {
+                    lat: 54.2264,
+                    lng: 37.6181
+                }
+            }, {
+                position: {
+                    lat: 54.2514,
+                    lng: 37.6336
+                }
+            }],
+            options: {
+                clickableIcons: false,
+                fullscreenControl: false,
+                minZoom: 11,
+                maxZoom: 18,
+                panControl: true,
+                rotateControl: false,
+                streetViewControl: false,
+                maptype: 'roadmap',
+            }
+        }
     }
-  }
 }
 </script>
-
 <style lang="css" scoped>
 </style>
